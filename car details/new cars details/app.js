d07f96c55ad8ae779863ad817ff340434beb7c2e
FIAKINGDRAGON1 <fainanjamil3893@gmail.com>
2026-01-23 20:43:43 +0500

    adding details and info

--- car details/new cars details/app.js
+++ car details/new cars details/app.js
@@ -5,6 +5,9 @@ const observer = new IntersectionObserver((entries) => {
     if (entry.isIntersecting) {
       entry.target.classList.add('show');
     }
+    else{
+      entry.target.classList.remove('show');
+    }
   });
 }, { threshold: 0.2 });
 
